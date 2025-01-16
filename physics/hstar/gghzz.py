48adf60415357effcb437edc55aeab09aa28badc
maxgriese <max.griese@tum.de>
2025-01-16 18:54:57 +0100

    Added Calculator objects and started refactoring  kinematics calculation and filtering

--- physics/hstar/gghzz.py
+++ physics/hstar/gghzz.py
@@ -11,15 +11,31 @@ class Events():
     self.weights = None
     self.probabilities = None
 
-  def filter(self, obj_instance):
-    indices, output = obj_instance.filter(self.kinematics, self.components, self.weights, self.probabilities)
+  def calculate(self, calculator):
+    events = Events()
+    events.kinematics = self.kinematics
+    events.components = self.components
+    events.weights = self.weights
+    events.probabilities = self.probabilities
 
-    self.kinematics = self.kinematics.take(indices)
-    self.components = self.components.take(indices)
-    self.weights = self.weights.take(indices)
-    self.probabilities = self.weights/self.weights.sum()
+    new_columns = calculator(events.kinematics)
 
-    return output
+    for column_name, column_series in new_columns.items():
+      events.kinematics[column_name] = column_series
+
+    return events
+
+  def filter(self, filter):
+    indices = filter(self.kinematics, self.components, self.weights, self.probabilities)
+
+    events = Events()
+
+    events.kinematics = self.kinematics.take(indices)
+    events.components = self.components.take(indices)
+    events.weights = self.weights.take(indices)
+    events.probabilities = events.weights/events.weights.sum()
+
+    return events
 
   def shuffle(self, random_state=None):
     events = Events()
@@ -31,24 +47,27 @@ class Events():
 
     return events
   
-  def split(self, train=0.5, validation=0.5, test=None):
-    if test is not None:
-      if train + validation + test <= 1.0:
-        split_1_kin, kinematics_test, split_1_comp, components_test, split_1_wt, weights_test = train_test_split(self.kinematics, self.components, self.weights, test_size=test, train_size=train+validation, shuffle=False)
-        kinematics_train, kinematics_val, components_train, components_val, weights_train, weights_val = train_test_split(split_1_kin, split_1_comp, split_1_wt, test_size=validation, train_size=train, shuffle=False)
+  def split(self, training=1, validation=1, testing=None):
+    if testing is not None:
+      total = training + validation + testing
+      training /= total
+      validation /= total
+      testing /= total
+        
+      split_1_kin, kinematics_test, split_1_comp, components_test, split_1_wt, weights_test = train_test_split(self.kinematics, self.components, self.weights, test_size=testing, train_size=training+validation, shuffle=False)
+      kinematics_train, kinematics_val, components_train, components_val, weights_train, weights_val = train_test_split(split_1_kin, split_1_comp, split_1_wt, test_size=validation, train_size=training, shuffle=False)
 
-        events_test = Events()
-        events_test.kinematics = kinematics_test
-        events_test.components = components_test
-        events_test.weights = weights_test
-        events_train.probabilities = weights_test/weights_test.sum()
-      else:
-        raise ValueError('Train, validation and test fractions must add up to 1.0')
+      events_test = Events()
+      events_test.kinematics = kinematics_test
+      events_test.components = components_test
+      events_test.weights = weights_test
+      events_train.probabilities = weights_test/weights_test.sum()
     else:
-      if train + validation <= 1.0:
-        kinematics_train, kinematics_val, components_train, components_val, weights_train, weights_val = train_test_split(self.kinematics, self.components, self.weights, test_size=validation, train_size=train, shuffle=False)
-      else:
-        raise ValueError('Train and validation fractions have must add up to 1.0')
+      total = training + validation
+      training /= total
+      validation /= total
+
+      kinematics_train, kinematics_val, components_train, components_val, weights_train, weights_val = train_test_split(self.kinematics, self.components, self.weights, test_size=validation, train_size=training, shuffle=False)
 
     events_train, events_val = Events(), Events()
     events_train.kinematics, events_val.kinematics = kinematics_train, kinematics_val
@@ -56,7 +75,7 @@ class Events():
     events_train.weights, events_val.weights = weights_train, weights_val
     events_train.probabilities, events_val.probabilities = weights_train/weights_train.sum(), weights_val/weights_val.sum()
 
-    if test is not None:
+    if testing is not None:
       return events_train, events_val, events_test
     else:
       return events_train, events_val
--- physics/hzz/zpair.py
+++ physics/hzz/zpair.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 from ..hstar import gghzz
 
-class ZPairChooser:
+class ZPairCandidate:
     def __init__(self, bounds1: tuple[int, int] = None, bounds2: tuple[int, int] = None, algorithm: str = 'leastsquare'):
         self.bounds1 = bounds1
         self.bounds2 = bounds2
@@ -17,6 +17,23 @@ class ZPairChooser:
 
         self.Z_mass = 91.18
     
+    def __call__(self, kinematics):
+        l1 = vector.array({'px': kinematics['p3_px'], 'py': kinematics['p3_py'], 'pz': kinematics['p3_pz'], 'E': kinematics['p3_E']})#negative l1
+        l2 = vector.array({'px': kinematics['p4_px'], 'py': kinematics['p4_py'], 'pz': kinematics['p4_pz'], 'E': kinematics['p4_E']})#positive l1
+        l3 = vector.array({'px': kinematics['p5_px'], 'py': kinematics['p5_py'], 'pz': kinematics['p5_pz'], 'E': kinematics['p5_E']})#negative l2
+        l4 = vector.array({'px': kinematics['p6_px'], 'py': kinematics['p6_py'], 'pz': kinematics['p6_pz'], 'E': kinematics['p6_E']})#positive l2
+
+        if self.algorithm == 'leastsquare':
+            return self.find_Z_lsq(l1, l2, l3, l4)
+        elif self.algorithm == 'closest':
+            return self.find_Z_closest(l1, l2, l3, l4)
+        elif self.algorithm == 'truth':
+            return {'l1_px': kinematics['p3_px'], 'l1_py': kinematics['p3_py'], 'l1_pz': kinematics['p3_pz'], 'l1_E': kinematics['p3_E'],
+                    'l2_px': kinematics['p4_px'], 'l2_py': kinematics['p4_py'], 'l2_pz': kinematics['p4_pz'], 'l2_E': kinematics['p4_E'],
+                    'l3_px': kinematics['p5_px'], 'l3_py': kinematics['p5_py'], 'l3_pz': kinematics['p5_pz'], 'l3_E': kinematics['p5_E'],
+                    'l4_px': kinematics['p6_px'], 'l4_py': kinematics['p6_py'], 'l4_pz': kinematics['p6_pz'], 'l4_E': kinematics['p6_E'],
+                    'Z1_mass': pd.Series((l1+l2).mass), 'Z2_mass': pd.Series((l1+l2).mass)}
+
     def find_Z_lsq(self, l1, l2, l3, l4):
         # Possible Z bosons from leptons 
         p12 = (l1 + l2)
@@ -49,7 +66,11 @@ class ZPairChooser:
         l1_1, l2_1 = lepton_pairs.transpose(3,0,1,2)[np.arange(len(closest_pair_indices)), closest_pair_indices][np.arange(len(pT_max_ind)), pT_max_ind].T
         l1_2, l2_2 = lepton_pairs.transpose(3,0,1,2)[np.arange(len(closest_pair_indices)), closest_pair_indices][np.arange(len(pT_min_ind)), pT_min_ind].T
 
-        return (l1_1, l2_1, l1_2, l2_2)
+        return {'l1_px': pd.Series(l1_1.px), 'l1_py': pd.Series(l1_1.py), 'l1_pz': pd.Series(l1_1.pz), 'l1_E': pd.Series(l1_1.E),
+                'l2_px': pd.Series(l2_1.px), 'l2_py': pd.Series(l2_1.py), 'l2_pz': pd.Series(l2_1.pz), 'l2_E': pd.Series(l2_1.E),
+                'l3_px': pd.Series(l1_2.px), 'l3_py': pd.Series(l1_2.py), 'l3_pz': pd.Series(l1_2.pz), 'l3_E': pd.Series(l1_2.E),
+                'l4_px': pd.Series(l2_2.px), 'l4_py': pd.Series(l2_2.py), 'l4_pz': pd.Series(l2_2.pz), 'l4_E': pd.Series(l2_2.E),
+                'Z1_mass': pd.Series((l1_1 + l2_1).mass), 'Z2_mass': pd.Series((l1_2 + l2_2).mass)}
     
     def find_Z_closest(self, l1, l2, l3, l4):
         # Possible Z bosons from leptons 
@@ -75,24 +96,27 @@ class ZPairChooser:
         l1_1, l2_1 = lepton_pairs.transpose(3,0,1,2)[np.arange(len(min_ind)), min_ind][np.arange(len(closest_Z_min_ind)), closest_Z_min_ind].T
         l1_2, l2_2 = lepton_pairs.transpose(3,0,1,2)[np.arange(len(min_ind)), min_ind][np.arange(len(closest_Z_max_ind)), closest_Z_max_ind].T
 
-        return (l1_1, l2_1, l1_2, l2_2)
+        return {'l1_px': pd.Series(l1_1.px), 'l1_py': pd.Series(l1_1.py), 'l1_pz': pd.Series(l1_1.pz), 'l1_E': pd.Series(l1_1.E),
+                'l2_px': pd.Series(l2_1.px), 'l2_py': pd.Series(l2_1.py), 'l2_pz': pd.Series(l2_1.pz), 'l2_E': pd.Series(l2_1.E),
+                'l3_px': pd.Series(l1_2.px), 'l3_py': pd.Series(l1_2.py), 'l3_pz': pd.Series(l1_2.pz), 'l3_E': pd.Series(l1_2.E),
+                'l4_px': pd.Series(l2_2.px), 'l4_py': pd.Series(l2_2.py), 'l4_pz': pd.Series(l2_2.pz), 'l4_E': pd.Series(l2_2.E),
+                'Z1_mass': pd.Series((l1_1 + l2_1).mass), 'Z2_mass': pd.Series((l1_2 + l2_2).mass)}
+    
 
-    def filter(self, kinematics, components, weights, probabilities) -> tuple[np.ndarray, tuple[vector.MomentumNumpy4D, vector.MomentumNumpy4D, vector.MomentumNumpy4D, vector.MomentumNumpy4D]]:
+class ZMasses():
+    def __init__(self, bounds1: tuple[int, int] = None, bounds2: tuple[int, int] = None):
+        self.bounds1 = bounds1
+        self.bounds2 = bounds2
+
+    def __call__(self, kinematics, components, weights, probabilities) -> tuple[np.ndarray, tuple[vector.MomentumNumpy4D, vector.MomentumNumpy4D, vector.MomentumNumpy4D, vector.MomentumNumpy4D]]:
         #Outgoing leptons
-        l1 = vector.array({'px': kinematics['p3_px'], 'py': kinematics['p3_py'], 'pz': kinematics['p3_pz'], 'E': kinematics['p3_E']})#negative l1
-        l2 = vector.array({'px': kinematics['p4_px'], 'py': kinematics['p4_py'], 'pz': kinematics['p4_pz'], 'E': kinematics['p4_E']})#positive l1
-        l3 = vector.array({'px': kinematics['p5_px'], 'py': kinematics['p5_py'], 'pz': kinematics['p5_pz'], 'E': kinematics['p5_E']})#negative l2
-        l4 = vector.array({'px': kinematics['p6_px'], 'py': kinematics['p6_py'], 'pz': kinematics['p6_pz'], 'E': kinematics['p6_E']})#positive l2
+        l1 = vector.array({'px': kinematics['l1_px'], 'py': kinematics['l1_py'], 'pz': kinematics['l1_pz'], 'E': kinematics['l1_E']})#negative l1
+        l2 = vector.array({'px': kinematics['l2_px'], 'py': kinematics['l2_py'], 'pz': kinematics['l2_pz'], 'E': kinematics['l2_E']})#positive l1
+        l3 = vector.array({'px': kinematics['l3_px'], 'py': kinematics['l3_py'], 'pz': kinematics['l3_pz'], 'E': kinematics['l3_E']})#negative l2
+        l4 = vector.array({'px': kinematics['l4_px'], 'py': kinematics['l4_py'], 'pz': kinematics['l4_pz'], 'E': kinematics['l4_E']})#positive l2
 
-        if self.algorithm == 'leastsquare':
-            l1_1, l2_1, l1_2, l2_2 = self.find_Z_lsq(l1, l2, l3, l4)
-        elif self.algorithm == 'closest':
-            l1_1, l2_1, l1_2, l2_2 = self.find_Z_closest(l1, l2, l3, l4)
-        elif self.algorithm == 'truth':
-            l1_1, l2_1, l1_2, l2_2 = l1, l2, l3, l4
-
-        Z1 = l1_1 + l2_1
-        Z2 = l1_2 + l2_2
+        Z1 = l1 + l2
+        Z2 = l3 + l4
 
         if self.bounds1 is not None:
             cond1 = np.where((Z1.mass>=self.bounds1[0])&(Z1.mass<=self.bounds1[1]))
@@ -107,9 +131,4 @@ class ZPairChooser:
         # Get only indices where cond1 and cond2 apply
         indices = np.intersect1d(cond1,cond2)
 
-        l1_1 = l1_1[indices]
-        l2_1 = l2_1[indices]
-        l1_2 = l1_2[indices]
-        l2_2 = l2_2[indices]
-
-        return (indices, (l1_1, l2_1, l1_2, l2_2))
+        return indices
