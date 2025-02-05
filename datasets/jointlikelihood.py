5c0429f857429bff3e1c3f89e2dc06e9e57bf095
maxicraftone <trashmco@gmail.com>
2025-02-05 10:37:32 +0100

    ALICE parameterization implementation (test)

--- datasets/jointlikelihood.py
+++ datasets/jointlikelihood.py
@@ -1,12 +1,17 @@
 import os, pickle
 
+import sys
+sys.path.insert(1,'..')
+
 from physics.simulation import msq, sample
 from physics.hzz import zpair, zz4l
+from physics.hstar import c6
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 import numpy as np
+import pandas as pd
 
 import lightning as L
 from torch.utils.data import DataLoader, TensorDataset, random_split, Dataset
@@ -14,7 +19,7 @@ import torch
 
 class AliceDataModule(L.LightningDataModule):
 
-    def __init__(self, filepath: str = '', features = ['cth_star', 'cth_1', 'cth_2', 'phi_1', 'phi', 'Z1_mass', 'Z2_mass', '4l_mass', '4l_rapidity'], numerator_component = msq.Component.SIG, denominator_component = msq.Component.BKG, scaler_path = 'scaler.pkl', sample_size = 10000, batch_size: int = 32, random_state: int=None) -> None:
+    def __init__(self, filepath: str = '', features = ['cth_star', 'cth_1', 'cth_2', 'phi_1', 'phi', 'Z1_mass', 'Z2_mass', '4l_mass', '4l_rapidity'], numerator_component = msq.Component.SIG, denominator_component = msq.Component.BKG, scaler_path = 'scaler.pkl', c6_values = None, sample_size = 10000, batch_size: int = 32, random_state: int=None) -> None:
         super().__init__()
 
         self.filepath = filepath
@@ -26,6 +31,7 @@ class AliceDataModule(L.LightningDataModule):
         self.random_state = random_state
         self.scaler = StandardScaler()
         self.scaler_path = scaler_path
+        self.c6_values = c6_values
     
     def prepare_data(self):
         sample_bkg = sample.from_csv(cross_section=1.0, file_path=self.filepath)
@@ -42,8 +48,12 @@ class AliceDataModule(L.LightningDataModule):
 
             sample_bkg_train, sample_bkg_val = self.sample_bkg.shuffle(random_state=self.random_state).split(train_size=0.5, val_size=0.5)
 
-            self.training_data = JointLikelihoodDataset(sample_bkg_train, features=self.features, numerator_component=self.numerator_component, denominator_component=self.denominator_component, sample_size=self.sample_size, random_state=self.random_state)
-            self.validation_data = JointLikelihoodDataset(sample_bkg_val, features=self.features, numerator_component=self.numerator_component, denominator_component=self.denominator_component, sample_size=self.sample_size, random_state=self.random_state)
+            if self.c6_values is None:
+                self.training_data = JointLikelihoodDataset(sample_bkg_train, features=self.features, numerator_component=self.numerator_component, denominator_component=self.denominator_component, sample_size=self.sample_size, random_state=self.random_state)
+                self.validation_data = JointLikelihoodDataset(sample_bkg_val, features=self.features, numerator_component=self.numerator_component, denominator_component=self.denominator_component, sample_size=self.sample_size, random_state=self.random_state)
+            else:
+                self.training_data = JointLikelihoodParameterizedDataset(sample_bkg_train, features=self.features, c6_values=self.c6_values, numerator_component=self.numerator_component, denominator_component=self.denominator_component, sample_size=self.sample_size, random_state=self.random_state)
+                self.validation_data = JointLikelihoodParameterizedDataset(sample_bkg_val, features=self.features, c6_values=self.c6_values, numerator_component=self.numerator_component, denominator_component=self.denominator_component, sample_size=self.sample_size, random_state=self.random_state)
 
             # Apply Scaler to both datasets after fitting to training data
             self.training_data.X = self.scaler.fit_transform(self.training_data.X)
@@ -70,6 +80,36 @@ class JointLikelihoodDataset(Dataset):
         r = sample.probabilities/sample.reweight(numerator=numerator_component, denominator=denominator_component).probabilities
 
         self.s = (1/(1 + r)).to_numpy()
+
+    def __len__(self):
+        return len(self.s)
+
+    def __getitem__(self, index):
+        return torch.tensor(self.X[index], dtype=torch.float32), torch.tensor(self.s[index], dtype=torch.float32)
+    
+class JointLikelihoodParameterizedDataset(Dataset):
+
+    def __init__(self, sample_bkg, features, c6_values, sample_size, numerator_component = msq.Component.SIG, denominator_component = msq.Component.BKG, random_state=None):
+        super().__init__()
+        c6_mod = c6.Modifier(baseline=numerator_component, sample=sample_bkg, c6_values=[-5,-1,0,1,5]) if numerator_component != msq.Component.INT else c6.Modifier(baseline=numerator_component, sample=sample_bkg, c6_values=[-5,0,5])
+        _, c6_probabilities = c6_mod.modify(c6_values)
+
+        X = np.tile(sample_bkg.kinematics[features].to_numpy(), (len(c6_values),1))
+        c6_column = np.repeat(c6_values, len(sample_bkg.kinematics), axis=0)[:,np.newaxis]
+
+        X = np.concatenate([X, c6_column], axis=1)
+
+        probabilities_numerator = c6_probabilities.reshape((c6_probabilities.shape[0]*c6_probabilities.shape[1],))
+        probabilities_denominator = np.tile(sample_bkg.probabilities.to_numpy(), len(c6_values))
+
+        sample_weights = pd.Series((probabilities_numerator + probabilities_denominator)/2*sample_size).reset_index(drop=True)
+        
+        unweighted_indices = sample_weights.sample(n=sample_size, replace=True, weights=sample_weights, random_state=random_state).index
+
+        s = 1/(1+probabilities_denominator/probabilities_numerator)
+
+        self.X = X[unweighted_indices]
+        self.s = s[unweighted_indices]
 
     def __len__(self):
         return len(self.s)
