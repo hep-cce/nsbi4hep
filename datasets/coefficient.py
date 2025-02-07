3a0403fa6f71f8b9c1f317a2728941b3d7dedc88
maxgriese <max.griese@tum.de>
2025-02-07 18:23:11 +0100

    Added scaler for NN outputs

--- datasets/coefficient.py
+++ datasets/coefficient.py
@@ -19,7 +19,7 @@ import torch
 
 class RolypolyDataModule(L.LightningDataModule):
 
-    def __init__(self, filepath: str = '', features = ['mandelstam_s', 'mandelstam_t', 'mandelstam_u'], coefficient_index=1, component = msq.Component.SBI, scaler_path = 'scaler.pkl', sample_size = 10000, batch_size: int = 32, random_state: int=None) -> None:
+    def __init__(self, filepath: str = '', features = ['mandelstam_s', 'mandelstam_t', 'mandelstam_u'], coefficient_index=1, component = msq.Component.SBI, X_scaler_path = 'scaler_X.pkl', y_scaler_path = 'scaler_y.pkl', sample_size = 10000, batch_size: int = 32, random_state: int=None) -> None:
         super().__init__()
 
         self.filepath = filepath
@@ -28,12 +28,14 @@ class RolypolyDataModule(L.LightningDataModule):
         self.sample_size = sample_size
         self.batch_size = batch_size
         self.random_state = random_state
-        self.scaler = StandardScaler()
-        self.scaler_path = scaler_path
+        self.X_scaler = StandardScaler()
+        self.X_scaler_path = X_scaler_path
+        self.y_scaler = StandardScaler()
+        self.y_scaler_path = y_scaler_path
         self.coefficient_index = coefficient_index
     
     def prepare_data(self):
-        sample = sample.from_csv(cross_section=1.0, file_path=self.filepath)
+        sample = events.from_csv(cross_section=1.0, file_path=self.filepath)
         
         z_cand = zpair.ZPairCandidate(algorithm='truth')
         z_masses = zpair.ZPairMassWindow(z1=(70,115), z2=(70,115))
@@ -50,10 +52,15 @@ class RolypolyDataModule(L.LightningDataModule):
             self.validation_data = CoefficientDataset(sample_val, features=self.features, coefficient_index=self.coefficient_index, component=self.component, sample_size=self.sample_size, random_state=self.random_state)
 
             # Apply Scaler to both datasets after fitting to training data
-            self.training_data.X = self.scaler.fit_transform(self.training_data.X)
-            with open(self.scaler_path, 'wb') as f:
-                pickle.dump(self.scaler, f)
-            self.validation_data.X = self.scaler.transform(self.validation_data.X)
+            self.training_data.X = self.X_scaler.fit_transform(self.training_data.X)
+            with open(self.X_scaler_path, 'wb') as f:
+                pickle.dump(self.X_scaler, f)
+            self.validation_data.X = self.X_scaler.transform(self.validation_data.X)
+
+            self.training_data.y = self.y_scaler.fit_transform(self.training_data.y)
+            with open(self.y_scaler_path, 'wb') as f:
+                pickle.dump(self.y_scaler, f)
+            self.validation_data.y = self.y_scaler.transform(self.validation_data.y)
             
     def train_dataloader(self):
         return DataLoader(self.training_data, batch_size=self.batch_size)
